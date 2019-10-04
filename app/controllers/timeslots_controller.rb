b075a805b215638898622d51145c50fa9546430e
VINCENT MOUTON <mouton.vincent@gmail.com>
2019-10-04 10:38:20 +0200

    Cleanup timeslot editing pages

--- app/controllers/timeslots_controller.rb
+++ app/controllers/timeslots_controller.rb
@@ -2,35 +2,24 @@ class TimeslotsController < ApplicationController
   before_action :authenticate
 
   # GET /timeslots
-  # GET /timeslots.xml
+  # GET /timeslots.json
   def index
     @timeslots = Timeslot.order('sorting')
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @timeslots }
-    end
-  end
-
-  # GET /timeslots/1
-  # GET /timeslots/1.xml
-  def show
-    @timeslot = Timeslot.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @timeslot }
+      format.json  { render :json => @timeslots }
     end
   end
 
   # GET /timeslots/new
-  # GET /timeslots/new.xml
+  # GET /timeslots/new.json
   def new
     @timeslot = Timeslot.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @timeslot }
+      format.json  { render :json => @timeslot }
     end
   end
 
@@ -40,46 +29,46 @@ class TimeslotsController < ApplicationController
   end
 
   # POST /timeslots
-  # POST /timeslots.xml
+  # POST /timeslots.json
   def create
     @timeslot = Timeslot.new(timeslot_params)
 
     respond_to do |format|
       if @timeslot.save
-        format.html { redirect_to(@timeslot, :notice => 'Timeslot was successfully created.') }
-        format.xml  { render :xml => @timeslot, :status => :created, :location => @timeslot }
+        format.html { redirect_to(timeslots_url) }
+        format.json  { render :json => @timeslot, :status => :created, :location => @timeslot }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @timeslot.errors, :status => :unprocessable_entity }
+        format.json  { render :json => @timeslot.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # PUT /timeslots/1
-  # PUT /timeslots/1.xml
+  # PUT /timeslots/1.json
   def update
     @timeslot = Timeslot.find(params[:id])
 
     respond_to do |format|
       if @timeslot.update_attributes(timeslot_params)
-        format.html { redirect_to(@timeslot, :notice => 'Timeslot was successfully updated.') }
-        format.xml  { head :ok }
+        format.html { redirect_to(timeslots_url) }
+        format.json  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @timeslot.errors, :status => :unprocessable_entity }
+        format.json  { render :json => @timeslot.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # DELETE /timeslots/1
-  # DELETE /timeslots/1.xml
+  # DELETE /timeslots/1.json
   def destroy
     @timeslot = Timeslot.find(params[:id])
     @timeslot.destroy
 
     respond_to do |format|
       format.html { redirect_to(timeslots_url) }
-      format.xml  { head :ok }
+      format.json  { head :no_content }
     end
   end
 
